f2c5437fb3b97953e21f22c3f8ee91654223aefa
Urval✨ <159251584+urval123@users.noreply.github.com>
2024-03-19 22:46:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const febHolidays = [
     "There's no one better than you💕",
     "You're the best Baby!!",
     "Or should I say, You are my lucky charm ",
-    "Wow!! Time flies, we've already been connected since we met online",
+    "Time flies, we've already been connected since we met online",
     "There's some hardships,",
     "but we overcame most of them",
     "and it made me realised how important you are in my life💕",
